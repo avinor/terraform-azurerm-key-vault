8f940519468ca750e2a591b87923e80ed4c72f44
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-09-20 15:40:45 +0200

    Added key vault resources

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = ">= 0.12.0"
+  required_providers {
+    azurerm = ">= 1.32.0"
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_key_vault" "main" {
+  name                = var.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = var.sku_name
+
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+
+  dynamic "network_acls" {
+    for_each = var.network_acls != null ? [true] : []
+    content {
+      bypass                     = var.network_acls.bypass
+      default_action             = var.network_acls.default_action
+      ip_rules                   = var.network_acls.ip_rules
+      virtual_network_subnet_ids = var.network_acls.virtual_network_subnet_ids
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_monitor_diagnostic_setting" "main" {
+  count                      = var.log_analytics_workspace_id != null ? 1 : 0
+  name                       = format("%s-analytics", var.name)
+  target_resource_id         = azurerm_key_vault.main.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  log {
+    category = "AuditEvent"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "main" {
+  count        = length(var.access_policies)
+  key_vault_id = azurerm_key_vault.main.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = var.access_policies[count.index].object_id
+
+  secret_permissions      = var.access_policies[count.index].secret_permissions
+  key_permissions         = var.access_policies[count.index].key_permissions
+  certificate_permissions = var.access_policies[count.index].certificate_permissions
+  storage_permissions     = var.access_policies[count.index].storage_permissions
+}
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "name" {
+  description = "Name of key vault account."
+}
+
+variable "resource_group_name" {
+  description = "Name of resource group to deploy resources in."
+}
+
+variable "location" {
+  description = "Azure location where resources should be deployed."
+}
+
+variable "sku_name" {
+  description = "The Name of the SKU used for this Key Vault. Possible values are `standard` and `premium`."
+  default     = "standard"
+}
+
+variable "enabled_for_deployment" {
+  description = "Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault. Defaults to `false`."
+  type        = bool
+  default     = false
+}
+
+variable "enabled_for_disk_encryption" {
+  description = "Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys. Defaults to `false`."
+  type        = bool
+  default     = false
+}
+
+variable "enabled_for_template_deployment" {
+  description = "Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault. Defaults to `false`."
+  type        = bool
+  default     = false
+}
+
+variable "access_policies" {
+  description = "Map of access policies for an object_id (user, service principal, security group) to backend."
+  type        = list(object({ object_id = string, certificate_permissions = list(string), key_permissions = list(string), secret_permissions = list(string), storage_permissions = list(string) }))
+  default     = []
+}
+
+variable "network_acls" {
+  description = "Network rules to apply to key vault."
+  type        = object({ bypass = string, default_action = string, ip_rules = list(string), virtual_network_subnet_ids = list(string) })
+  default     = null
+}
+
+variable "log_analytics_workspace_id" {
+  description = "Specifies the ID of a Log Analytics Workspace where Diagnostics Data should be sent."
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
+}
